e6e2b8948bbbfb2f69d14d6b08387aebed235533
theNerd247 <noah.harvey247 gm>
2019-07-24 17:23:46 -0400

    (split)moves src/ to schedule/

---
+++ src/makeSchedule.js
@@ -0,0 +1,21 @@
+const { generateScheduleFromJSONFile } = require('./parseSchedule.js')
+const { importJSONFile, compose } = require('./util.js')
+const { eventsToMarkdownTable } = require('./eventsToMarkdown.js')
+const { toICal } = require('./ical.js')
+const { makeCourseOverview } = require('./courseoverview.js')
+
+const outputType = () => process.argv[2]
+
+const configFilePath = () => process.argv[3]
+
+const configFileName = () => configFilePath().replace(/.json/, '')
+
+const toOutput = (type) =>
+  type.match(/markdown|md/i) ?  eventsToMarkdownTable
+  : type.match(/ical|calendar|ics/i) ?  toICal(configFileName())
+  : type.match(/overview/i) ? compose(makeCourseOverview, toICal(configFileName()))
+  : throwErr(`invalid output type ${type}`)
+
+const makeScheduleFromCli = 
+  generateScheduleFromJSONFile(configFilePath())
+  .then(toOutput(outputType()))
